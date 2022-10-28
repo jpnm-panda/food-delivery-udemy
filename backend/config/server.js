3e194a1c6b2e3b956b5f808af75d72a3c0c3b1c0
jpnm-panda <jpnm0415shkm@gmail.com>
2022-10-29 06:54:34 +0900

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'c9a6728167fab390a19c8459e19ed5d0'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6fbb384f-33fd-4075-9c3c-fb4605e1415e'
+};
